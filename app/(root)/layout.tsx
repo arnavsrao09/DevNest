e06d679156d73d37bd5424030e4257ba66cddce4
Arnav S Rao <arnavsrao09@gmail.com>
2026-03-02 23:55:56 +0530

    Update the basic global css styles

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "../globals.css";
+import LightRays from "@/components/LightRays";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -27,8 +28,22 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
-        <div>NavBar</div>
-        {children}
+        <div className="absolute inset-0 z-[-1] min-h-screen top-0">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfdca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.5}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0}
+            distortion={0}
+          />
+        </div>
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
+    <section>
+      <h1 className='text-center'>The Hub for Every Dev <br /> Event You Can't Miss</h1>
+      <p className='text-center mt-5'>Hackathons, Meetups, and Conferences, All In One Place</p>
+    </section>
   )
 }
 
 export default page
+ 
