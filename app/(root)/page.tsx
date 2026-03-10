0d64bc4c3f0dd99ec16fbe4693e3e37848155c24
Arnav S Rao <arnavsrao09@gmail.com>
2026-03-10 23:48:39 +0530

    Fix the event card styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
       {/* Featured Events section */}
       <div className='mt-7 space-y-7'>
         <h3>Featured Events</h3>
-        <ul className='flex list-none justify-evenly'>
+        <ul className='flex list-none gap-x-6'>
           {upcomingEvents.map((event) => (
             <li key={event.title}>
               <EventCard {...event} />
