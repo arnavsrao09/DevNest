dbb60b9b0977c707c41b9626f8709236a5251d6d
Arnav S Rao <arnavsrao09@gmail.com>
2026-03-11 23:52:26 +0530

    add description in about page

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,7 +1,8 @@
 const page = () => {
   return (
-    <div>
-      About
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1>About Us</h1>
+      <p>We are a team of developers who are passionate about creating a platform for developers to find events and connect with other developers.</p>
     </div>
   )
 }
