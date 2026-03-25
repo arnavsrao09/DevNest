4c0879c9a2b35ec0c1510bc23d0c5537908a3351
Arnav S Rao <arnavsrao09@gmail.com>
2026-03-25 20:43:17 +0530

    Add fetching and logging logic for individual event page

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const EventDetailsPage = () => {
+const EventDetailsPage = async({params} : { params: Promise<{slug: string}>}) => {
+  const { slug } = await params;
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/events/${slug}`);
+  const data = await response.json();
+  const event = data.event;
+  console.log(event);
   return (
     <div>
       
--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -10,9 +10,9 @@ interface Props {
   time: string
   location: string
 }
-const EventCard = ({ title, image, location, date ,time}: Props) => {
+const EventCard = ({ title, image, location, date ,time, slug}: Props) => {
   return (
-    <Link href={`/events`} id='event-card'>
+    <Link href={`/events/${slug}`} id='event-card'>
         <Image src={image} alt={title} width={410} height={300} />
         <div className='flex flex-row gap-2'>
           <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
